a88f7fabcd419ef8e4106829f2bada06b046a27b
Rohan Mia <rohanmia.org@gmail.com>
2026-05-20 13:11:24 +0600

    mongodb connected to the server

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
 const express = require('express')
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const dotenv = require('dotenv')
 const cors = require('cors')
-
+dotenv.config()
 const app = express()
 app.use(cors())
 app.use(express.json())
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8000
+const uri = process.env.MONGODB_URI;
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+      await client.connect();
+      
+      const db = client.db('reserva')
+      const facilityCollection = db.collection('facilities')
+    
+      app.get('/facilities', (req, res) => {
+          res.send("all facilities will show here")
+      })
+
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Server is running fine!");
