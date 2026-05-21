ec3eac076a4f496a2815040bf9cafa3ac575a22d
Rohan Mia <rohanmia.org@gmail.com>
2026-05-21 11:39:36 +0600

    delete operation added for bookings page

--- index.js
+++ index.js
@@ -64,6 +64,14 @@ async function run() {
      console.log(result);
      res.send(result);
    });
+    
+    app.delete("/bookings/:id", async (req, res) => {
+      const id = req.params.id
+      console.log(id)
+      const result = await bookingCollection.deleteOne({ _id: new ObjectId(id) })
+      console.log(result)
+      res.send(result)
+    })
 
 
 
